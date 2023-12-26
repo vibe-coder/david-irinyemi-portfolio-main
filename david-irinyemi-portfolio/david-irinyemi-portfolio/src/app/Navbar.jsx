5a879e00ec0ac0ab53aed2d2488a9082342f7dbf
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-26 23:38:30 +0100

    Created NavBar component

---
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+
+const NavBar = () => {
+  return(
+    <nav className="px-8 py-5 border-b border-white flex justify-between" >
+      <h1 className="text-white text-xl">DI.</h1>
+
+      <div className="bg-red">
+        <div>burger</div>
+        <div>Info</div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
