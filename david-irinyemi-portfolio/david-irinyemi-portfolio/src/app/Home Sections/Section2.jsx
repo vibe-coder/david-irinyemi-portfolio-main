27bce80648a6716f63cc1ee8e1bf27c9fde13ac1
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-28 16:14:27 +0100

    Configured font to work sans and removed height 100vh

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Home Sections/Section2.jsx
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Home Sections/Section2.jsx
@@ -1,9 +1,15 @@
+import { Work_Sans } from 'next/font/google'
+
+const worksans = Work_Sans({ 
+  subsets: ['latin'],
+  // display: 'swap'
+})
 
 
 
 const Section2 = () => {
   return(
-    <section className="px-8 h-screen">
+    <section className="px-8 py-24">
       <div className="text-text-gray">
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime dignissimos, ipsam iure velit exercitationem commodi quibusdam esse minus earum fuga soluta quia distinctio, error doloribus adipisci hic quaerat rem consectetur. <br/> <br/> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus aspernatur eveniet nemo, aperiam hic fuga quibusdam labore non exercitationem maiores et magni a quam placeat. Obcaecati fugit ipsam quo adipisci.</p>
         <button className="border-2 border-text-gray px-5 py-2 rounded-3xl text-text-gray mt-6 font-bold">CONTACT ME</button>
