6c10e0dd894698fce07e10d0c54de18515e718f6
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-28 16:15:10 +0100

    Imported and rendred Footer

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/page.js
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import HomeContainer from './HomeContainer'
 import NavBar from './Navbar'
+import Footer from './Footer'
 
 export default function Home() {
   return (
     <section className='bg-gray overflow-x-hidden h-screen'>
       <NavBar/>
       <HomeContainer/>
+      <Footer/>
     </section>
   )
 }
