367792edcf4add77b99befa2da4e076582f7d362
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-29 16:38:52 +0100

    Removed unused style property

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/page.js
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/page.js
@@ -5,7 +5,7 @@ import Footer from './Footer'
 
 export default function Home() {
   return (
-    <section className='bg-peach overflow-x-hidden h-screen'>
+    <section className='bg-peach'>
       <HomeContainer/>
     </section>
   )
