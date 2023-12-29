510bf1b0b975896183924862b668d1c6d17da002
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-29 09:37:35 +0100

    Added short description, changed color, chnaged arrow icon, adjusted scroll div height

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Home Sections/Section1.jsx
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Home Sections/Section1.jsx
@@ -1,29 +1,29 @@
 import './Styles/Section1.scss'
-import { Work_Sans } from 'next/font/google'
-
-const worksans = Work_Sans({ 
-  subsets: ['latin'],
-  // display: 'swap'
-})
 
 
 const Section1 = () => {
   return(
-    <section className="pt-5 h-[80vh]">
+    <section className="pt-5 h-[85vh]">
       <div id='wrapper' className='h-full'>
-        <div id='scroll' className="overflow-scroll">
+        <div id='scroll' className='font h-[33rem]'>
           <div id='to-left' className="mt-[-30px] h-[14rem]">
-            <h1 className="text-[14rem]  text-text-gray font-extrabold m-0">Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer</h1>
+            <h1 className="text-[14rem]  text-text-gray font-extrabold m-0 uppercase">Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer</h1>
           </div>
 
           <div id='to-right' className=''>
-            <h1 className="text-[14rem] m-0 text-text-gray font-extrabold">Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer Frontend Developer</h1>
+          <h1 className="text-[14rem]  text-text-gray font-extrabold m-0 uppercase">Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer - Frontend Developer</h1>
           </div>
         </div>
 
-        <div className='font px-8 flex justify-between items-center'>
-          <p className='text-text-gray'>David Irinyemi</p>
-          <p className='text-4xl rotate-90 text-text-gray'>&#10139;</p>
+        <div className='px-8'>
+          <p className='text-burnt-orange text-center font-sans font-normal text-lg'><span className='font-bold'>Creative Frontend Developer</span> specializing in designing & developing user-friendly and easy-to-navigate user platforms.</p>
+        </div>
+
+        <div id='scroll-down-div' className='font px-8 flex flex-col justify-between items-center mt-10'>
+            <p className='font-sans font-bold text-sm text-text-gray'>Scroll Down</p>
+            <div className='h-10 w-10 border border-text-gray rounded-3xl mt-2 flex justify-center items-start overflow-hidden'>
+              <p id='arrow' className='text-3xl text-text-gray'>&#8675;</p>
+            </div>
         </div>
       </div>
     </section>
