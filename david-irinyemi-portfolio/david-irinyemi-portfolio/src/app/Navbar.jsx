51a8a8a880ba5332230bec84cac90ed2a0304ca9
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-29 09:34:37 +0100

    Changed colour of text and border to burnt orange

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Navbar.jsx
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Navbar.jsx
@@ -9,8 +9,8 @@ const worksans = Work_Sans({
 
 const NavBar = () => {
   return(
-    <nav className="py-5 w-screen border-b-2 font-sans border-text-gray flex justify-between relative">
-      <h1 className="text-gray-light text-xl ml-8">DI.</h1>
+    <nav className="py-5 w-screen border-b-2 font-sans border-burnt-orange flex justify-between relative">
+      <h1 className="text-burnt-orange text-xl ml-8">DI.</h1>
 
       <div className="right-section-wrapper">
         <input id='input' type="checkbox" className='absolute right-8 z-[1]'/>
@@ -21,7 +21,7 @@ const NavBar = () => {
           <p className='text-md font-bold'>M</p>
         </div>
         
-        <div id='menu-list' className="menu-list border-l-2 border-b-2 border-text-gray text-gray-light flex-1 h-[245px] w-[90%] md:w-[50%] lg:w-[30%] right-0 top-[50px] absolute z-[3] px-8 py-8 flex flex-col gap-8 bg-gray">
+        <div id='menu-list' className="menu-list border-l-2 border-b-2 border-burnt-orange text-burnt-orange flex-1 h-[245px] w-[90%] md:w-[50%] lg:w-[30%] right-0 top-[50px] absolute z-[3] px-8 py-8 flex flex-col gap-8 bg-peach">
           <a href="/">About Me</a>
           <a href="/">Projects</a>
           <a href="/">Resume</a>
