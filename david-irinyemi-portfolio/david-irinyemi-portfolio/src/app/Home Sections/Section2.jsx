4e25b4a1a2338748a35addb9cc7090b6d2b49c2b
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-29 09:56:11 +0100

    Chnaged font weight, button text and border colour

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Home Sections/Section2.jsx
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Home Sections/Section2.jsx
@@ -1,29 +1,21 @@
-import { Work_Sans } from 'next/font/google'
-
-const worksans = Work_Sans({ 
-  subsets: ['latin'],
-  // display: 'swap'
-})
-
-
 
 const Section2 = () => {
   return(
     <section className="px-8 py-24">
       <div className="text-text-gray">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime dignissimos, ipsam iure velit exercitationem commodi quibusdam esse minus earum fuga soluta quia distinctio, error doloribus adipisci hic quaerat rem consectetur. <br/> <br/> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus aspernatur eveniet nemo, aperiam hic fuga quibusdam labore non exercitationem maiores et magni a quam placeat. Obcaecati fugit ipsam quo adipisci.</p>
-        <button className="border-2 border-text-gray px-5 py-2 rounded-3xl text-text-gray mt-6 font-bold">CONTACT ME</button>
+        <p className='font-sans font-normal'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime dignissimos, ipsam iure velit exercitationem commodi quibusdam esse minus earum fuga soluta quia distinctio, error doloribus adipisci hic quaerat rem consectetur. <br/> <br/> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus aspernatur eveniet nemo, aperiam hic fuga quibusdam labore non exercitationem maiores et magni a quam placeat. Obcaecati fugit ipsam quo adipisci.</p>
+        <button className="border-2 border-burnt-orange px-5 py-2 rounded-3xl text-burnt-orange mt-6 font-bold tracking-wider">CONTACT ME</button>
       </div>
 
-      <div className="mt-10">
+      <div className="mt-14">
         <div className="bg-text-gray text-text-gray h-[26rem]">
           This is picture holder
         </div>
 
         <div className="flex mt-9 justify-between">
-          <button className="border-2 border-text-gray px-4 py-2 rounded-3xl text-text-gray text-sm font-bold">ABOUT ME</button>
-          <button className="border-2 border-text-gray px-4 py-2 rounded-3xl text-text-gray">PROJECTS</button>
-          <button className="border-2 border-text-gray px-4 py-2 rounded-3xl text-text-gray">RESUME</button>
+          <button className="border-2 border-burnt-orange px-5 py-2 rounded-3xl text-burnt-orange font-bold tracking-wider">ABOUT ME</button>
+          <button className="border-2 border-burnt-orange px-5 py-2 rounded-3xl text-burnt-orange tracking-wider">PROJECTS</button>
+          <button className="border-2 border-burnt-orange px-5 py-2 rounded-3xl text-burnt-orange tracking-wider">RESUME</button>
         </div>
       </div>
     </section>
