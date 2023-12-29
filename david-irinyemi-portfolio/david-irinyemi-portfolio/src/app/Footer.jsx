f235cb54567671a752c20b7fdc6a833cd12d63b1
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-29 14:38:37 +0100

    Increased height, added margin botton to copyright div, added href link to social media anchors

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Footer.jsx
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Footer.jsx
@@ -8,27 +8,27 @@ const worksans = Work_Sans({
 
 const Footer = () => {
   return(
-    <section className="px-8 pb-12 text-text-gray h-[70vh] flex items-end border-t-2 border-text-gray">
+    <section className="px-8 pb-12 text-text-gray h-[80vh] flex items-end border-t-2 border-transparent">
       <div className='w-full'>
         <div>
-          <h1 className='font tracking-widest'>SAY HELLO</h1>
-          <p className='pt-3'>thevibecoder@gmail.com</p>
+          <h1 className='font tracking-widest fon'>SAY HELLO</h1>
+          <p className='pt-3 text-burnt-orange'>thevibecoder@gmail.com</p>
         </div>
 
-        <div className='pt-14 flex flex-col gap-6'>
+        <div className='pt-14 flex flex-col gap-6 text-burnt-orange'>
           <p>ABOUT ME</p>
           <p>PROJECTS</p>
           <p>RESUME</p>
         </div>
 
         <div className='flex gap-5 pt-14'>
-          <button className='border-2 border-text-gray rounded-3xl h-10 w-10 font-bold'>IN</button>
-          <button className='border-2 border-text-gray rounded-3xl h-10 w-10 font-bold'>GT</button>
-          <button className='border-2 border-text-gray rounded-3xl h-10 w-10 font-bold'>X</button>
+          <button className='border-2 border-burnt-orange text-burnt-orange  rounded-3xl h-10 w-10 font-bold'><a href='https://www.linkedin.com/in/david-irinyemi-395220162/' target='_blank' className='font-bold'>IN</a></button>
+          <button className='border-2 border-burnt-orange text-burnt-orange  rounded-3xl h-10 w-10 font-bold'><a href='https://github.com/vibe-coder' target='_blank' className='font-bold'>GT</a></button>
+          <button className='border-2 border-burnt-orange text-burnt-orange  rounded-3xl h-10 w-10 font-bold'><a href='https://twitter.com/vibe_coder' target='_blank' className='font-bold'>X</a></button>
         </div>
 
-        <div className='pt-24 w-full'>
-          <p>@ David Irinyemi 2023</p>
+        <div className='pt-24 w-full mb-10'>
+          <p>© David Irinyemi 2023</p>
         </div>
       </div>
     </section>
