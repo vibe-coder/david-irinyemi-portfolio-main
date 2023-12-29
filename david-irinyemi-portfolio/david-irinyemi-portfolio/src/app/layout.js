b495e4a71c584682d95301207adb460e22e5a4e6
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-29 09:38:18 +0100

    Imported addtional fonts

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/layout.js
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/layout.js
@@ -1,12 +1,18 @@
-import { Inter, Work_Sans } from 'next/font/google'
+import { Inter, Work_Sans, Silkscreen, Staatliches, Big_Shoulders_Display } from 'next/font/google'
 
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const worksans = Work_Sans({ 
+const staatliches = Staatliches({ 
   subsets: ['latin'],
+  weight: '400'
   // display: 'swap'
+})
+
+const big_shoulders_display = Big_Shoulders_Display({
+  subsets: ['latin'],
+  weight:'900'
 })
 
 export const metadata = {
@@ -17,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={worksans.className}>{children}</body>
+      <body className={big_shoulders_display.className}>{children}</body>
     </html>
   )
 }
