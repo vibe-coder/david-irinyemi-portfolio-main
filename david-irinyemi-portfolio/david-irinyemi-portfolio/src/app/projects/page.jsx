1f068a76811755e7e5f4b479fbce695cec65a9b1
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-31 21:55:39 +0100

    Configured Projects component for large screen

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/projects/page.jsx
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/projects/page.jsx
@@ -12,16 +12,16 @@ const Projects = () => {
       </div>
 
       <div className="px-8 flex justify-center">
-        <p className="text-center font-sans font-normal md:text-2xl md:w-[85%]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, placeat perspiciatis deleniti fuga libero aliquid tenetur fugit sequi ex corporis id debitis nam dolor a rem optio! Natus, beatae quasi?</p>
+        <p className="text-center font-sans font-normal md:text-2xl md:w-[85%] lg:w-[60%]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, placeat perspiciatis deleniti fuga libero aliquid tenetur fugit sequi ex corporis id debitis nam dolor a rem optio! Natus, beatae quasi?</p>
       </div>
 
       <div id='scroll-down-div' className='font px-8 flex flex-col justify-between items-center mt-20'>
         <div className='h-10 w-10 border border-burnt-orange rounded-3xl mt-2 flex justify-center items-start overflow-hidden'>
           <p id='arrow' className='text-3xl text-burnt-orange'>&#8675;</p>
         </div>
-      </div>
+      </div> 
 
-      <div className="px-8 py-20 grid grid-cols-1 md:grid-cols-2 gap-7 md:px-16 md:pt-28">
+      <div className="px-8 py-20 grid grid-cols-1 md:grid-cols-2 gap-7 md:px-16 md:pt-28 lg:px-80">
         <div className="h-80 flex justify-center items-center bg-text-gray hover:bg-burnt-orange cursor-pointer transition duration-300 ease-in-out">
           <a href="https://vibe-mymoviebox.netlify.app/" target="_blank" className="text-4xl text-peach w-full h-full flex justify-center items-center">MOVIEBOX</a>
         </div>
