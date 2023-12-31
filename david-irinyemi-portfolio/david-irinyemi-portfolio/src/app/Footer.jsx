8306a010d54eec42148109a79467683168b04165
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-12-31 21:39:31 +0100

    Changed justification to center

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Footer.jsx
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/Footer.jsx
@@ -2,27 +2,27 @@
 
 const Footer = () => {
   return(
-    <section className="px-8 pb-12 text-text-gray h-[80vh] flex items-end border-t-2 border-transparent bg-peach md:h-[70vh] md:px-16 lg:px-24 lg:h-[100vh]">
-      <div className='w-full'>
-        <div>
-          <h1 className='tracking-widest md:text-2xl'>SAY HELLO</h1>
-          <a href="mailto:thevibecoder@gmail.com" className='pt-3 text-burnt-orange underline md:text-2xl animation duration-200 ease-in-out hover:text-text-gray'>thevibecoder@gmail.com</a>
+    <section className="px-8 pb-12 text-text-gray h-[80vh] flex items-end justify-center border-t-2 border-transparent md:h-[70vh] md:px-16 lg:px-24 lg:h-[100vh]">
+      <div className='w-full flex flex-col items-center'>
+        <div className="flex flex-col items-center">
+          <h1 className='tracking-widest text-xl md:text-2xl'>SAY HELLO</h1>
+          <a href="mailto:thevibecoder@gmail.com" className=' text-burnt-orange underline text-xl md:text-2xl animation duration-200 ease-in-out hover:text-text-gray'>thevibecoder@gmail.com</a>
         </div>
 
-        <div className='pt-14 flex flex-col gap-6 text-burnt-orange'>
-        <a href="/about" className='transition duration-200 ease-in-out w-[40%] hover:text-text-gray md:text-2xl'>About Me</a>
-        <a href="/projects" className='transition duration-200 ease-in-out w-[40%] hover:text-text-gray md:text-2xl'>Projects</a>
-        <a href="/" className='transition duration-200 ease-in-out w-[40%] hover:text-text-gray md:text-2xl'>Resume</a>
+        <div className='flex flex-row justify-center gap-6 mt-16 md:gap-20 md:w-[30%] text-burnt-orange md:mt-20'>
+          <a href="/about" className='transition duration-200 ease-in-out hover:text-text-gray md:text-2xl text-xl'>About Me</a>
+          <a href="/projects" className='transition duration-200 ease-in-out hover:text-text-gray md:text-2xl text-xl'>Projects</a>
+          <a href="/" className='transition duration-200 ease-in-out hover:text-text-gray md:text-2xl text-xl'>Resume</a>
         </div>
 
-        <div className='flex gap-5 pt-14'>
-          <button className='border-2 border-burnt-orange text-burnt-orange rounded-3xl h-10 w-10 font-bold md:h-16 md:rounded-full md:w-16 md:text-2xl animation duration-200 ease-in-out hover:text-text-gray hover:border-text-gray'><a href='https://www.linkedin.com/in/david-irinyemi-395220162/' target='_blank' className='font-bold'>IN</a></button>
-          <button className='border-2 border-burnt-orange text-burnt-orange  rounded-3xl h-10 w-10 font-bold md:h-16 md:rounded-full md:w-16 md:text-2xl animation duration-200 ease-in-out hover:text-text-gray hover:border-text-gray'><a href='https://github.com/vibe-coder' target='_blank' className='font-bold'>GT</a></button>
-          <button className='border-2 border-burnt-orange text-burnt-orange  rounded-3xl h-10 w-10 font-bold md:h-16 md:rounded-full md:w-16 md:text-2xl animation duration-200 ease-in-out hover:text-text-gray hover:border-text-gray'><a href='https://twitter.com/vibe_coder' target='_blank' className='font-bold'>X</a></button>
+        <div className='flex gap-5 md:mt-7 mt-5'>
+          <button className='border-2 border-burnt-orange text-burnt-orange rounded-3xl h-10 w-10 font-bold md:h-12 md:rounded-full md:w-12 md:text-2xl animation duration-200 ease-in-out hover:text-text-gray hover:border-text-gray'><a href='https://www.linkedin.com/in/david-irinyemi-395220162/' target='_blank' className='font-bold'>IN</a></button>
+          <button className='border-2 border-burnt-orange text-burnt-orange  rounded-3xl h-10 w-10 font-bold md:h-12 md:rounded-full md:w-12 md:text-2xl animation duration-200 ease-in-out hover:text-text-gray hover:border-text-gray'><a href='https://github.com/vibe-coder' target='_blank' className='font-bold'>GT</a></button>
+          <button className='border-2 border-burnt-orange text-burnt-orange  rounded-3xl h-10 w-10 font-bold md:h-12 md:rounded-full md:w-12 md:text-2xl animation duration-200 ease-in-out hover:text-text-gray hover:border-text-gray'><a href='https://twitter.com/vibe_coder' target='_blank' className='font-bold'>X</a></button>
         </div>
 
-        <div className='pt-24 w-full mb-10'>
-          <p className="md:text-2xl">© David Irinyemi 2023</p>
+        <div className='w-full mb-10 flex justify-center md:mt-20 mt-16'>
+          <p className="md:text-2xl text-xl">© David Irinyemi 2023</p>
         </div>
       </div>
     </section>
