9e98b92d3f49504475453a43c46f7c9b47a7b34a
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2024-01-09 14:20:24 +0100

    Reduced Projects text size, reduced description text size, increased space between description and moving arrow

--- david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/projects/page.jsx
+++ david-irinyemi-portfolio/david-irinyemi-portfolio/src/app/projects/page.jsx
@@ -6,16 +6,16 @@ const Projects = () => {
       <div id='wrapper'>
         <div id='scroll'>
           <div id='to-left' className="md:mt-[-30px]">
-            <h1 className="text-[14rem] text-text-gray font-extrabold uppercase md:text-[20rem]"> PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS</h1>
+            <h1 className="text-[14rem] text-text-gray font-extrabold uppercase md:text-[14rem]"> PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS - PROJECTS</h1>
           </div>
         </div>
       </div>
 
       <div className="px-8 flex justify-center">
-        <p className="text-center font-sans font-normal md:text-2xl md:w-[85%] lg:w-[60%]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, placeat perspiciatis deleniti fuga libero aliquid tenetur fugit sequi ex corporis id debitis nam dolor a rem optio! Natus, beatae quasi?</p>
+        <p className="text-center font-sans font-normal md:text-2xl lg:text-xl text-burnt-orange md:w-[85%] lg:w-[60%]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, placeat perspiciatis deleniti fuga libero aliquid tenetur fugit sequi ex corporis id debitis nam dolor a rem optio! Natus, beatae quasi?</p>
       </div>
 
-      <div id='scroll-down-div' className='font px-8 flex flex-col justify-between items-center mt-20'>
+      <div id='scroll-down-div' className='font px-8 flex flex-col justify-between items-center mt-20 lg:mt-52'>
         <div className='h-10 w-10 border border-burnt-orange rounded-3xl mt-2 flex justify-center items-start overflow-hidden'>
           <p id='arrow' className='text-3xl text-burnt-orange'>&#8675;</p>
         </div>
